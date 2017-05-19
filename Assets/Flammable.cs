beef87cd70f8877b9c4f626013bdf4dbb8deb559
Standby88 <32767517@student.murdoch.edu.au>
2017-05-19 14:10:38 +0800

    ABE Fire extinguisher working
    the inital fire extinguisher works now uses a particle collision
    technique to work

---
+++ Assets/Flammable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flammable : MonoBehaviour {
+
+    public ParticleSystem partSystem;
+    int count;
+    private void Start()
+    {
+        count = 0;
+        //print("test");
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        count++;
+        //print("particle collision detected " + count);
+        if (count >= 100)
+        {
+            print("emissionrate dropped");
+            partSystem.emissionRate = partSystem.emissionRate - 1.0f;
+
+            count = 0;
+        }
+
+    }
+}
